2c07f48754c9b42cc4f57965acf28e254ec7a895
TsapkoZh <60929704+TsapkoZh@users.noreply.github.com>
2021-03-01 01:38:16 +0300

    added header

---
+++ components/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="whiteLine">
+    <img 
+      src="~/assets/images/Aplen-gold-logo 2.png"
+      alt="logo"
+      class="logo"
+    />
+  </div>
+</template>
+
+<style scoped>
+.whiteLine {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  height: 6.7vh;
+  background-color: #ffffff;
+}
+
+.logo {
+  height: 7.2rem;
+  transform: translateY(50%);
+}
+</style>
